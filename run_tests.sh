d96f96078c620c303588209a8c8cd2a37912520a
Adrien Chauve <adrien.chauve@gmail.com>
2011-06-16 22:16:57 +0200

    Add another run-test command to dump the AST

--- run_tests.sh
+++ run_tests.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
 
-clang -cc1 -load build-debug/libcppanalyze.so -plugin rename -x c++ -std=c++98 -fcxx-exceptions -fexceptions tests/test.cpp
+clang  -cc1 -load build-debug/libcppanalyze.so -plugin rename -x c++ -std=c++98 -fcxx-exceptions -fexceptions tests/test.cpp
+
+## uncomment to dump the AST in addition to run cppanalyze plugin
+#clang  -cc1 -ast-dump -load build-debug/libcppanalyze.so -add-plugin rename -x c++ -std=c++98 -fcxx-exceptions -fexceptions tests/test.cpp
