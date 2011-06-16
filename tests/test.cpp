00be4df9fb547355f847d591d4b76d52b07d0f5a
Adrien Chauve <adrien.chauve@gmail.com>
2011-06-16 22:17:18 +0200

    Add another macro test case

--- tests/test.cpp
+++ tests/test.cpp
@@ -4,6 +4,9 @@
 #include <vector>
 #include <cassert>
 
+#define TEST_MACRO(x)\
+    x.int_bar = 5;
+
 void Foo::setBar(double v)
 {
     double_bar = v;
@@ -17,6 +20,8 @@ int main()
     double_foo.int_bar = 2;
 
     assert(double_foo.int_bar == 2);
+
+    TEST_MACRO(double_foo)
 
     Foo foo;
     foo.double_bar = 2.3;
